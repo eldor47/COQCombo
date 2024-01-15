721d044a46124eed5ed6e38d240dd409a82b02e9
eldor47 <eldor474747@gmail.com>
2024-01-15 00:25:41 -0800

    Fix twitter link things

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css'
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Unity, useUnityContext } from "react-unity-webgl";
 
 function App() {
@@ -10,15 +10,34 @@ function App() {
     codeUrl: "assets/Build/assets.wasm",
   });
 
+  const [width, setWidth] = useState(window.innerWidth);
+
+  function handleWindowSizeChange() {
+      setWidth(window.innerWidth);
+  }
+
+
+  useEffect(() => {
+      window.addEventListener('resize', handleWindowSizeChange);
+      return () => {
+          window.removeEventListener('resize', handleWindowSizeChange);
+      }
+  }, []);
+  
+  const isMobile = width <= 768;
+
   return (
-    <div style={{display: "flex", justifyContent: 'center', alignItems: 'center', width: "100vw"}}>
-      {!isLoaded && (
-        <div style={{textAlign: 'center', fontSize: '20px'}}><p>Loading Application... {Math.round(loadingProgression * 100)}%</p></div>
-      )}
-      <Unity
-        unityProvider={unityProvider}
-        style={{ visibility: isLoaded ? "visible" : "hidden", aspectRatio: "16 / 9", height: window.innerHeight, width: window.innerWidth }}
-      />
+    <div style={{display: "flex", justifyContent: 'center', alignItems: 'center', width: "100vw", textAlign: 'center'}}>
+      {isMobile ? <div>Mobile is not supported. Please play on a Desktop / Laptop device. Android / iOS in the works!</div> :
+      <>
+        {!isLoaded && (
+          <div style={{width: '500px', textAlign: 'center'}}>Loading Application... {Math.round(loadingProgression * 100)}%</div>
+        )}
+        <Unity
+          unityProvider={unityProvider}
+          style={{ visibility: isLoaded ? "visible" : "hidden", aspectRatio: "16 / 9", height: window.innerHeight, width: window.innerWidth }}
+        />
+      </>}
     </div>
   );
 }
